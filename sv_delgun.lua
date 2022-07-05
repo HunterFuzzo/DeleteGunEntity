feb0b2a836c8f4a949b59c00b8744e8622e05af1
Azuka <100121407+HunterFuzzo@users.noreply.github.com>
2022-07-05 19:23:40 +0100

    Update sv_delgun.lua

--- sv_delgun.lua
+++ sv_delgun.lua
@@ -1,36 +1,30 @@
--- check if the player has permission
+ESX = nil
 
-Citizen.CreateThread(function()
-	while ESX == nil do
-		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		Citizen.Wait(0)
-	end
-end)
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
 
 RegisterCommand("delgun", function(source, args)
-        local _src = source
-        local xPlayer = ESX.GetPlayerFromId(_src)
-	local playerGroup = xPlayer.getGroup() 
-	
-	if args[1] == nil then
-		ESX.ShowNotification("~r~An argument is missing")
-		return
-	end
+    local _src = source
+    local xPlayer = ESX.GetPlayerFromId(_src)
+	   local playerGroup = xPlayer.getGroup() 
+	   
+	   if playerGroup ~= "user" then 
+	       if args[1] == nil then
+		          ESX.ShowNotification("~r~An argument is missing")
+		          return
+	       end
+        
+	       if args[1] ~= "on" or "off" then
+		          ESX.ShowNotification("~r~Argument is incorrect or invalid")
+		          return
+	       end
 
-	if args[1] ~= "on" or "off" then
-		ESX.ShowNotification("~r~Argument is incorrect or invalid")
-		return
-	end
+	       if args[1] == "on" then
+			        TriggerClientEvent('fine:delgunOn', _src)
+		      end
 
-	if playerGroup ~= "user" then
-		if args[1] == "on" then
-			TriggerClientEvent('fine:delgunOn', _src)
-		end
-
-		if args[1] == "off" then
-			TriggerClientEvent('fine:delgunOff', _src)
-		end
-	else
-		CancelEvent()
-	end
+		      if args[1] == "off" then
+			         TriggerClientEvent('fine:delgunOff', _src)
+		      end 
+	   end
 end)
