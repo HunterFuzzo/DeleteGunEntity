00851836f7c90f4bd0f312fb02ce0c1039211ff7
Azuka <100121407+HunterFuzzo@users.noreply.github.com>
2023-01-01 17:09:06 +0100

    Update sv_delgun.lua

--- sv_delgun.lua
+++ sv_delgun.lua
@@ -5,25 +5,28 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 RegisterCommand("delgun", function(source, args)
     local _src = source
     local xPlayer = ESX.GetPlayerFromId(_src)
-	   local playerGroup = xPlayer.getGroup() 
-	   
-	   if playerGroup ~= "user" then 
-	       if args[1] == nil then
-		          ESX.ShowNotification("~r~An argument is missing")
-		          return
-	       end
-        
-	       if args[1] ~= "on" or "off" then
-		          ESX.ShowNotification("~r~Argument is incorrect or invalid")
-		          return
-	       end
+    local playerGroup = xPlayer.getGroup() 
 
-	       if args[1] == "on" then
-			        TriggerClientEvent('fine:delgunOn', _src)
-		      end
+    if playerGroup ~= "admin" and playerGroup ~= "superadmin" then 
+        ESX.ShowNotification("~r~You don't have the necessary permissions")
+        return
+    end
 
-		      if args[1] == "off" then
-			         TriggerClientEvent('fine:delgunOff', _src)
-		      end 
-	   end
+    if #args ~= 1 then
+        ESX.ShowNotification("~r~An argument is missing or there are too many arguments")
+        return
+    end
+
+    if args[1] ~= "on" and args[1] ~= "off" then
+        ESX.ShowNotification("~r~Argument is incorrect or invalid")
+        return
+    end
+
+    if args[1] == "on" then
+        TriggerClientEvent('fine:delgunOn', _src)
+    end
+
+    if args[1] == "off" then
+        TriggerClientEvent('fine:delgunOff', _src)
+    end 
 end)
